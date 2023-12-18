f582cbe6e13a0f6969cb17cf9a7049008bd957ce
giustiniman <justointer@gmail.com>
2023-12-18 17:43:25 +0100

    sdk updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.speedypizza"
-        minSdk = 26
+        minSdk = 28
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -75,7 +75,7 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.1")
 
 
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
     implementation("com.google.android.gms:play-services-mlkit-text-recognition-common:19.0.0")
     coreLibraryDesugaring ("com.android.tools:desugar_jdk_libs:<latest-version>")
 
@@ -85,7 +85,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.1")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -107,12 +107,12 @@ dependencies {
 //    implementation ("androidx.compose.material:material:1.0.0")
 //    implementation ("androidx.compose.foundation:foundation:1.0.0")
     implementation("com.android.tools:desugar_jdk_libs:2.0.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:2.7.6")
     implementation("io.coil-kt:coil-compose:2.4.0")
     implementation("androidx.compose.material3:material3:1.1.2")
-    implementation ("androidx.navigation:navigation-compose:2.7.5")
+    implementation ("androidx.navigation:navigation-compose:2.7.6")
 
 
 }
